2c82efb31252189598bbc824bffd01e8dfac4a68
Hilquias Braz <hlqsss8@gmail.com>
2025-09-23 16:52:49 -0300

    Implementação de função para adicionar amigos

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let nomeAmigoSecreto = [];
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function adicionarAmigo() {
+    let nome = document.querySelector('input').value;
+    console.log(nome);
+    if (nome.trim() === '') {
+        document.getElementById('verificacao').removeAttribute('hidden');
+        limparCampo();
+    } else {
+        nomeAmigoSecreto.push(nome);
+        document.getElementById('verificacao').setAttribute('hidden', true);
+        limparCampo();
+    }
+}
+
+function limparCampo() {
+    nome = document.querySelector('input');
+    nome.value = '';
+}
+console.log('Teste')
+function adicaoDeNomes() {
+
+}
