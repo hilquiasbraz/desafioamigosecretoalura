3a1b76c8b8d807606db40161b57a0de822c4bb36
Hilquias Braz <hlqsss8@gmail.com>
2025-09-23 20:03:20 -0300

    Implementação de função para atualizar a lista de amigos

--- app.js
+++ app.js
@@ -8,21 +8,31 @@ function exibirTextoNaTela(tag, texto) {
 function adicionarAmigo() {
     let nome = document.querySelector('input').value;
     console.log(nome);
+    limparCampo();
     if (nome.trim() === '') {
         document.getElementById('verificacao').removeAttribute('hidden');
-        limparCampo();
     } else {
         nomeAmigoSecreto.push(nome);
+        listaDeAmigos(nome);
         document.getElementById('verificacao').setAttribute('hidden', true);
-        limparCampo();
     }
 }
 
 function limparCampo() {
-    nome = document.querySelector('input');
+    let nome = document.querySelector('input');
     nome.value = '';
 }
-console.log('Teste')
-function adicaoDeNomes() {
 
+function listaDeAmigos(nome) {
+    let amigos = document.getElementById('lista');
+    amigos.innerHTML = '';
+
+    for(let amigo of nomeAmigoSecreto) {
+        let lista = document.createElement('li');
+        lista.textContent = amigo;
+        amigos.appendChild(lista);
+    }
+    // let lista = document.createElement('li');
+    // lista.textContent = nome;
+    // amigos.appendChild(lista);
 }
